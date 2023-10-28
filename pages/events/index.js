af70afb3f7ebc86c0dbde7475607e098b14bca09
Saurabh Singh <saurabhsingh121@gmail.com>
2023-10-28 07:46:57 +0530

    first commit

---
+++ pages/events/index.js
@@ -0,0 +1,23 @@
+import { useRouter } from "next/router";
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
+import { Fragment } from "react";
+function AllEventsPage() {
+    const allEvents = getAllEvents();
+    const router = useRouter();
+
+    function findEventsHandler(year, month) {
+        const fullPath = `/events/${year}/${month}`;
+        router.push(fullPath);
+    }
+
+    return (
+        <Fragment>
+            <EventsSearch onSearch={findEventsHandler} />
+            <EventList items={allEvents} />
+        </Fragment>
+    );
+}
+
+export default AllEventsPage;
